bad60438f00ca06acd08be9c5741edd8fc7867e1
EffessDev <faseeh1080@gmail.com>
2026-06-11 21:08:31 +0530

    fix redirects and protect home using proxy

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -6,8 +6,17 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import SignInWithGitHub from "@/components/ui/sign-in-with-github"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const session = await auth.api.getSession({ headers: await headers() })
+
+  if (session !== null) {
+    redirect("/home")
+  }
+
   return (
     <div className="flex min-h-dvh w-full flex-col items-center justify-center p-4">
       <Card className="min-w-sm">
