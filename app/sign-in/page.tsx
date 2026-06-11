c70ca08ffb48e74846aa360fbb8ecdab11de53de
EffessDev <faseeh1080@gmail.com>
2026-06-11 20:16:16 +0530

    update sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import SignInWithGitHub from "@/components/ui/sign-in-with-github"
 
 export default function SignInPage() {
-  return <SignInWithGitHub />
+  return (
+    <div className="flex h-dvh w-full flex-col items-center justify-center p-4">
+      <Card className="min-w-sm">
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+          <CardDescription>
+            Sign in using one of the methods given below.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInWithGitHub />
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
