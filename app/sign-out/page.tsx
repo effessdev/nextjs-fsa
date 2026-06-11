4b67a130fbcc5f2d1c0ae340737e1704b510a746
EffessDev <faseeh1080@gmail.com>
2026-06-11 21:20:09 +0530

    bug: fix signed out toast appearing twice

--- app/sign-out/page.tsx
+++ app/sign-out/page.tsx
@@ -3,18 +3,23 @@
 import { Spinner } from "@/components/ui/spinner"
 import { authClient } from "@/lib/auth-client"
 import { useRouter } from "next/navigation"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { toast } from "sonner"
 
 export default function SignOutPage() {
   const router = useRouter()
   const [message, setMessage] = useState("")
+  const signedOutSuccessfully = useRef(false)
 
   useEffect(() => {
     async function signOut() {
       await authClient.signOut({
         fetchOptions: {
           onSuccess: () => {
+            if (signedOutSuccessfully.current) return
+
+            signedOutSuccessfully.current = true
+
             toast.success("Signed out successfully")
             router.push("/")
           },
