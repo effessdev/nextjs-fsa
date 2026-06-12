fb0fa0f3abfd98bf1da1cdf1ff1722d2301adf7a
EffessDev <faseeh1080@gmail.com>
2026-06-12 18:31:41 +0530

    update text-db page

--- app/test-db/page.tsx
+++ app/test-db/page.tsx
@@ -12,6 +12,14 @@ export default async function TestDb() {
   }
 
   return (
-    <p>{dbIsWorking ? "Database is working" : "Database is not working"}</p>
+    <div className="flex min-h-dvh w-full flex-col items-center justify-center p-4">
+      <p className="max-w-sm text-center text-sm text-muted-foreground">
+        Using @neondatabase/serverless, we tried to run SELECT 1 on the
+        database.
+        {dbIsWorking
+          ? " It didn't throw an error, so the database is probably working properly."
+          : " It threw an error, so the database is probably not working properly."}
+      </p>
+    </div>
   )
 }
